284bac3b968b7ceeaef186554e637fc6f046b042
benhamdiayoub <ayouja@hotmail.fr>
2018-12-03 11:14:27 +0000

    Replace menu.h

--- game/menu.h
+++ game/menu.h
@@ -1,32 +1,37 @@
 #ifndef __MENU_H
 #define __MENU_H
-#include"window.h>"
-
+#include "window.h"
 
 class Menu {
 
 private:
-        Window field ; 
+          Window menu;  
 
  public :
+
 	// constructeur par défaut et parametré de menu
-	menu();
-	menu(window field);
+	Menu();
+
+	Menu(Window &w);
 	//accesseur en lecture
-	Color getBKgdColorField()const;
-	Color getBordColorField()const; 
-	int GetHeightFiled()const;
-	int GetWidthField()const;
-	int getYField()const;
-	int getXField()const;
+	Color getBKgdColorMenu()const;
+	Color getBordColorMenu()const; 
+	int GetHeightMenu()const;
+	int GetWidthMenu()const;
+	int getYMenu()const;
+	int getXMenu()const;
 
 	// accsseur en ecriture
-	void setBkgColorField(Color);
-	void setBordColorField(Color);
-       	void setHeightField(int H);
-	void setWidhtField(int W);
-	void setBordureFile(char c);
+	void setBkgColorMenu(Color);
+	void setBordColorMenu(Color);
+       	void setHeightMenu(int H);
+	void setWidhtMenu(int W);
+	void setBordureMenu(char c);
 
 	//methodes
+
+	void printStriingMenu(int x, int y, std::string s);
+	
 	
 };
+#endif 

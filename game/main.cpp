ece0b482d4921c0e0611ac93b50c56cfd2a51d08
sentes <antoine.afflatet@gmail.com>
2018-12-12 18:37:47 +0100

    Version final du 12/12

--- game/main.cpp
+++ game/main.cpp
@@ -5,11 +5,44 @@
 #include"balle.h"
 #include"menu.h"
 using namespace std;
+
 void GlobalPrint(Ball &b,terrain &ter,racket &r){
   ter.clearField();
   printRacket(r,ter);
   ter.printInField(b.getposX(),b.getposY(),b.getChar(),WBLACK);
 }
+
+void GlobalMove(Ball &b, terrain &ter, racket &r, bool &leftM, bool &rightM){
+  
+  b.move_Ball();
+  
+  if (leftM) {
+
+    if (((r.getXRacket()-(ter.getXField()-2))<=4)){
+	
+	r.setPosXRacket(ter.getXField()-2);
+	
+      } else if((r.getXRacket()-(ter.getXField()-2))>4){
+	r.setPosXRacket(r.getXRacket()-2);
+      }
+  } else if (rightM) {
+
+    if ((( ((ter.getXField()-2) + ter.getWidthField()) ) - (r.getXRacket()+r.getWidthRacket())) > 4 ){
+	
+	r.setPosXRacket(r.getXRacket()+2);
+	
+      } else if((((ter.getXField()-2)+ter.getWidthField())-(r.getXRacket()+r.getWidthRacket()))<=4){
+	
+	r.setPosXRacket((ter.getXField() + ter.getWidthField())-(r.getWidthRacket()+3));
+      }
+  } 
+}
+
+void GlobalCollision(Ball &b, terrain &ter, racket &r){
+  r.collision_Ball_racket(b);
+  ter.collision_Ball_field(b);
+}
+
 void myProgram(){
   Window w(30,70,2,2,'_');
   Menu menu;
@@ -19,55 +52,54 @@ void myProgram(){
   ter.setBordColorField(BWHITE);
   racket r1(8,35,26);
   int ch ;
- 
- 
-    while((ch = getch()) != 'q')
-      {
-	  b.move_Ball();
-	  r1.collision_Ball_racket(b);
-	  ter.collision_Ball_field(b);
-	  GlobalPrint(b,ter,r1);
-	  
-	switch (ch) {
-	case '1':
-	  ter.setBkgdColorField(WCYAN);
-	  break;
-	case '2':
-	  ter.setBkgdColorField(WRED);
-	  break;
-	case '3':
-	  ter.setBordureField('X');
-	break;
-	case '4' :
-	  ter.setBkgdColorField(WBLACK);
-	  break;
-	case KEY_UP:
-	
-	  break;
-	case KEY_LEFT:
-	  if (((r1.getXRacket()-(ter.getXField()-2))<=4)){
-	    
-	    r1.setPosXRacket(ter.getXField()-2);
+  bool leftMouvRacket = false;
+  bool rightMouvRacket = false;
+  
+   
+  while((ch = getch()) != 'q'){
 
-	  } else if((r1.getXRacket()-(ter.getXField()-2))>4){
-	    r1.setPosXRacket(r1.getXRacket()-4);
-	  }
-	  GlobalPrint(b,ter,r1);
-	  break;
-	case KEY_RIGHT:
-	  
-	  if ((( ((ter.getXField()-2) + ter.getWidthField()) ) - (r1.getXRacket()+r1.getWidthRacket())) > 4 ){
-	    
-	    r1.setPosXRacket(r1.getXRacket()+4);
+   
+    GlobalMove(b,ter,r1,leftMouvRacket, rightMouvRacket);
+    GlobalCollision(b,ter,r1);
+    GlobalPrint(b,ter,r1);
 
-	  } else if((((ter.getXField()-2)+ter.getWidthField())-(r1.getXRacket()+r1.getWidthRacket()))<=4){
-	    
-	    r1.setPosXRacket((ter.getXField() + ter.getWidthField())-(r1.getWidthRacket()+3));
-	  }
-	  GlobalPrint(b,ter,r1);
-	  break;
-	}
-      }    
+   
+    switch (ch) {
+    case '1':
+      ter.setBkgdColorField(WCYAN);
+      break;
+    case '2':
+      ter.setBkgdColorField(WRED);
+      break;
+    case '3':
+      ter.setBordureField('X');
+      break;
+    case '4' :
+      ter.setBkgdColorField(WBLACK);
+      break;
+    case KEY_LEFT:
+      if (leftMouvRacket==false){
+	leftMouvRacket=true;
+      } else if (leftMouvRacket==true){
+	leftMouvRacket=false;
+      }
+      rightMouvRacket=false;
+      break;
+    case KEY_RIGHT:
+      if (rightMouvRacket==false){
+	rightMouvRacket=true;
+      } else if (rightMouvRacket==true){
+	rightMouvRacket=false;
+      }
+      leftMouvRacket=false;
+      break;
+    case ' ':
+      leftMouvRacket=false;
+      rightMouvRacket=false;
+      break;
+   
+    }
+  }    
 }
 
 int main(){
